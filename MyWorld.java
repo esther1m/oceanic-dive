3bf677aa283198af7a9722e68b68e4febe908e82
esther1m <146996363+esther1m@users.noreply.github.com>
2023-12-10 11:57:07 +0000

    instantiates shipwreck pieces at execution

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    private static final int NUMBER_OF_SHIPWRECK = 7;
+    private static final int NUMBER_OF_SHIPWRECK = 6;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,17 +16,17 @@ public class MyWorld extends World
     public MyWorld()
     {    
         
+        
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); 
         populate();
     }
         
-    public void populate(){
-
+  private void populate(){
+        
         for (int i = 0; i < NUMBER_OF_SHIPWRECK; i++){
             int x = Greenfoot.getRandomNumber(getWidth());
             addObject(new shipwreckWood(), x, 4);
         }
-
     }
 }
