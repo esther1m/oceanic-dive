59c4f50c3993d109c12ab29b179be4639f0c45ba
esther1m <146996363+esther1m@users.noreply.github.com>
2023-12-10 11:57:07 +0000

    instantiates shipwreck pieces at execution

--- MyWorld.java
+++ MyWorld.java
@@ -19,7 +19,6 @@ public class MyWorld extends World
     public MyWorld()
     {    
         
-        
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); 
         populate();
