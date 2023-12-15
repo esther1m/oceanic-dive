2d78cf770552b3d1bb827fd5715353b9ec0aa74a
esther1m <146996363+esther1m@users.noreply.github.com>
2023-12-15 12:31:42 +0000

    adding all the code into this branch

--- MyWorld.java
+++ MyWorld.java
@@ -17,7 +17,7 @@ public class MyWorld extends World
     {    
         
         
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a new world with 1000x600 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); 
         populate();
     }
