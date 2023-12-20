9b5374d1d304aba0a27470b2419e89bb95cbd91c
esther1m <146996363+esther1m@users.noreply.github.com>
2023-12-20 20:11:46 +0000

    fixing seaweed movement

--- seaweed.java
+++ seaweed.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class seaweed extends Actor
+public class Seaweed extends Actor
 {
     /**
      * Act - do whatever the seaweed wants to do. This method is called whenever
