d2b5c4a2ecc6f6e5803e31cdaa7168c82b5551d7
muhammadfazil123 <22233584@bucks.ac.uk>
2023-12-20 16:52:54 +0000

    edit made to Creatures health (3 hits to kill it as opposed to 1 hit)

--- Creature.java
+++ Creature.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Creature extends Actor
 {
-    int health = 1;
+    int health = 3;
     DiverShooter player;
     Counter counter;
     public Creature (DiverShooter mainPlayer, Counter counter)
