4a0204aaf447d68d520380995d0acbe59dbe0a1a
muhammadfazil123 <22233584@bucks.ac.uk>
2024-01-04 18:26:59 +0000

    edit

--- Counter.java
+++ Counter.java
@@ -24,14 +24,21 @@ public class Counter extends Actor
         setImage(new GreenfootImage("Score: " + score + "\n Energy: " + energy + "\n Time: " + time/60, 40, Color.WHITE, new Color (0,0,0,0)));  
         youWin();
     }
+    //
     public void youWin()
     {
+        /* if the time in seconds is equal to 180 the code will excectue,
+        text will appear on screen with instructions for the gamer to follow, 
+        there is a greenfoot delay so that the game is essentially paused,
+        */ 
         if (time/60 == 180)
         {
-            getWorld().showText("Final score: " + score + "\n \n Press Z to see what is next... ", getWorld().getWidth()/2 , getWorld().getHeight()/2);
+            getWorld().showText("Final score: " + score + "\n \n Click the mouse to see what is next... ", getWorld().getWidth()/2 , getWorld().getHeight()/2);
             Greenfoot.delay(100);
         }
-        if (Greenfoot.isKeyDown("Z") && time/60 >= 180)
+        /*if the mouse is pressed and the time is greater than or equal to 
+        then the code will execute and take you to the win screen*/
+        if (Greenfoot.mousePressed(null) && time/60 >= 180)
         {
             Greenfoot.setWorld(new YouWinScreen());
         }
